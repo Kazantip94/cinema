399d703b2998d1662cd49a1a6a5d8159305e0881
Serhii <blackdato@gmail.com>
2021-09-23 18:27:44 +0300

    add banners

---
+++ src/components/banners/BannerUpper.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="banner">
+        <div class="banner-item">
+            <i class="fas fa-window-close"></i>
+            <img class="img-upper" src="" alt="">
+            <div class="div">
+                <img 
+                :src="avatar" 
+                alt="Image" 
+                height="150">
+                <input type="file" @change="GetImage">
+            </div>
+            <div class="inputs-group">
+                <div class="inputs">
+                    <label for="url">URL:</label>
+                    <input/>
+                </div>
+                <div class="inputs">
+                    <label for="url">Текст:</label>
+                    <input />
+                </div>
+            </div>
+        </div>     
+    </div>
+</template>
+
+<script>
+
+
+export default ({
+    name: 'BannerUpper',
+    data () {
+        return {
+            avatar: null
+        }
+    },
+    methods: {
+        GetImage(e) {
+            let image = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e => {
+                this.avatar = e.target.result
+            }
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+    .banner {
+        width: 200px;
+        height: 300px;
+        margin: 0 15px;
+    }
+    .banner-item {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -3,31 +3,27 @@
     <div class="container-fluid"> 
       <div class="row">
         <div class="col-md-12">
+          <h1>На главной верх</h1>
           <div class="banner-upper">
-            <h1>На главной верх</h1>
             <div class="size">
               <span class="top">Размер: 1000х190</span>
-              <button class="on-off"></button>
+              <button class="btn btn-outline-secondary on-off">ON/OFF</button>
             </div>
-            <ul class="list-default flex">
-              <li>
-                <banner-card />
-              </li>
-            </ul>
+            <BannerUpper />
             <div class="bottom">
               <span class="speed">Скорость вращения</span>
-              <select>
-                <option>test</option>
+              <select class="form-control">
+                <option>5с</option>
               </select>
-              <button class="btns"></button>
+              <button class="btn btn-outline-secondary btns">Cохранить</button>
             </div>
 
           </div>
+          <h2>Сквозной банер на заднем фоне</h2>
           <div class="banner-back">
-            <h1>Сквозной банер на заднем фоне</h1>
           </div>
-          <div class="banner-stock">
-            <h1>На главной Новости Акции</h1>
+          <h3>На главной Новости Акции</h3>
+          <div class="banner-stock"> 
              <div class="size">
               <span class="top">Размер: 1000х190</span>
               <button class="on-off"></button>
@@ -51,22 +47,60 @@
   </section>
 </template>
 
+<script>
+import BannerUpper from '../components/banners/BannerUpper'
+
+export default ({
+
+  components: {
+    BannerUpper
+  }
+  
+})
+</script>
+
 
 <style lang="scss">
   .banner-upper {
     height: 500px;
     border: 1px solid black;
     margin: 50px 50px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .size {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px 30px;
+  }
+  .bottom {
+    display: flex;
+    align-items: center;
+    margin: 30px 90px;
+  }
+  .speed {
+    margin-right: 30px;
+  }
+  .btns {
+    margin-left: 70px;
+  }
+  .form-control {
+    width: 70px;
   }
   .banner-back {
     height: 200px;
     border: 1px solid black;
     margin: 50px 50px;
+    border-radius: 15px;
   }
   .banner-stock {
     height: 300px;
     border: 1px solid black;
     margin: 0 50px;
+    border-radius: 15px;
   }
 
 </style>
